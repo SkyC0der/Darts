c303736fc5adf6d1ef6c799be221c2aa4bc58765
Emmanuel Ezenwigbo <emmanuelezenwigbo@gmail.com>
2020-05-08 14:29:59 +0100

    Created Darts function

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const score = (x, y) => {
-    //type code here
+  //  Equation of Circle is x^2 + y^2 = r^2
+  if (isNaN(x) || isNaN(y)){return null};
+  let radius = Math.sqrt(x*x + y*y );
+ return (radius <= 1) ? 10 : (radius <= 5) ? 5 : (radius <= 10) ? 1 : 0; 
   }
 
 module.exports = score;
